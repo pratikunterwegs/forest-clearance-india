a1c53a2f891f6136d9639b7b14a7a7b2dd8b5c86
Pratik Gupte <pratikgupte16@gmail.com>
2020-07-09 07:20:35 +0200

    remove old data

--- code/code_02_ndvi_series.r
+++ code/code_02_ndvi_series.r
@@ -4,9 +4,11 @@ library(stringr)
 library(tidyr)
 library(ggplot2)
 library(scico)
+library(glue)
+library(lubridate)
 
 # read data
-data <- read_csv("Data/landsat/sample_ndvi.csv")
+data <- read_csv("Data/landsat/landsat8_ndvi_series_2014_2019.csv")
 
 # select cols
 data <- select(data,
@@ -27,21 +29,30 @@ data <- mutate(data,
                ndvi = if_else(ndvi == -9999, NA_real_, ndvi))
 
 # remove NA ndvi
-data <- filter(data,
-               !is.na(ndvi))
+data <- drop_na(data, ndvi)
+
+# group by month
+data_summary <- data %>% 
+  mutate(month_year = glue('{month(date, label = TRUE)}/{year(date)}'),
+         month_year = as_date(parse_date_time(month_year, orders = c("m/Y")))) %>% 
+  group_by(id, month_year) %>% 
+  summarise(ndvi = mean(ndvi))
 
 # plot
-ggplot(data,
-       aes(x = date, y = ndvi,
+ggplot(data_summary,
+       aes(x = month_year, y = ndvi,
            group = id)) +
-  geom_point(col = "steelblue", 
-             alpha = 0.1) +
-  geom_smooth(se = F, size = 0.3,
-              col = "steelblue") +
+  geom_jitter(col = "steelblue",
+             alpha = 1,
+             size = 0.2) +
+  geom_line(se = F, size = 0.3,
+            alpha = 0.5,
+            col = "steelblue") +
   scale_x_date(date_labels = "%B %Y") +
   scale_fill_scico(palette = "bilbao",
                      direction = 1) +
-  coord_cartesian(ylim = c(-0.1, 1.0)) +
+  coord_cartesian(ylim = c(-0.1, 1.0),
+                  expand = T) +
   theme_test(base_family = "IBM Plex Sans")+
   theme(legend.position = c(0.8, 0.1),
         legend.direction = "horizontal",
@@ -57,3 +68,5 @@ ggplot(data,
   labs(x = "Time",
        y = "NDVI",
        colour = "NDVI")
+
+ggsave("Figures/fig_sample_ndvi.png", dpi = 300)
